7401b8d0fc454e87ee6e4e42ec77c2debdfea6b5
Alchemist <Ripple@Alchemist>
2025-08-02 17:41:06 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <>
+      <div className='text-sm border-2 border-green-400'>
+        This is div1
+      </div>
+
+      <div className='text-sm'>
+        This is div2
+      </div>
+
+      <div className='text-sm'>
+        This is div3
+      </div>
+    </>
+  )
+}
+
+export default App
